e20a6f9898d215860c97c104c2bcc160bdd79166
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-20 19:54:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,9 @@
 def prime?(n)
-  range = (2..n-1).to_a
-  if range > 1
-    range.none? do |prime|
-      range % prime == 0
+  start = 2
+  if number > 0
+   range = (start..number-1).to_a
+     range.none? do |prime|
+      number % prime == 0
     end
   else
     false
