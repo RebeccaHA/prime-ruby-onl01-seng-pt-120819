7be9f3677aad72088578efa56f51a578ab875c8d
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-20 19:51:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 def prime?(n)
   start = 2
-if number >= 1
-  range = (start..number-1).to_a
-  range.none? do |prime|
-    number % prime == 0
+  if number > 1
+   range = (start..number-1).to_a
+     range.none? do |prime|
+      number % prime == 0
+    end
+  else
+    false
   end
-else
-  false
-end
 end
   
   
