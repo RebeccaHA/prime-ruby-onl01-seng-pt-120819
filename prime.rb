ba97be631bd2a3b99c195c0e423d888fb34c9e37
calm-block-7153 <rebeccahunt034@gmail.com>
2019-12-20 19:29:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,13 @@
 def prime?(n)
-  (2..(n-1)).each do |num|
-   if 
+ prime = (2..(n-1))
+  prime.each do |num|
+    if 
      num % num == 0
      puts "true"
     else 
       puts "false"
     end
+  prime
   end
 end
   
